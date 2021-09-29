15d1df7c342d91c48318bc2b7623e2f0ea96dc67
jy374 <90633673+jy374@users.noreply.github.com>
2021-09-29 11:21:29 -0400

    Update servers.php

--- servers.php
+++ servers.php
@@ -6,3 +6,16 @@ $back_server = "25.52.190.242"; // yi-hsuan
 $database_server = "25.52.70.25"; // jakub
 
 $back_server_creds = ["backend", "back"];
+
+// RABBIT MQ USER ACCOUNT INFO //
+
+//USER:backend
+//PASSWORD: back
+
+//USER: frontend
+//PASSWORD: front
+
+//USER: database
+//PASSWORD: data
+
+    
