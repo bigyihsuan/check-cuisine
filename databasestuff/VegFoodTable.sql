a88d5c3640942c8bca6a183e55a2196a534fa805
jr635 <70586888+jr635@users.noreply.github.com>
2021-09-29 10:58:55 -0400

    Create VegFoodTable.sql

---
+++ databasestuff/VegFoodTable.sql
@@ -0,0 +1,248 @@
+/* CREATE TABLE */
+CREATE TABLE vegfoodinfo (
+Food VARCHAR(100),
+Serving_Size INTEGER,
+Calories INTEGER,
+Fat INTEGER,
+Sugar INTEGER,
+Carbs INTEGER,
+Protein INTEGER,
+Fiber INTEGER
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Artichoke', 100, 47, 0, 3, 10, 5, 0
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Asparagus', 100, 20, 0, 2, 3, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Green Beans', 100, 31, 0, 1, 6, 2, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Beets', 100, 43, 0, 1, 9, 2, 6
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Broccoli', 100, 34, 0, 2, 6, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Brussel Sprouts', 100, 43, 0, 3, 8, 3, 2
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Green Cabbage', 100, 25, 0, 1, 5, 2, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Red Cabbage', 100, 31, 0, 1, 7, 2, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Carrot', 100, 41, 0, 0, 9, 2, 4
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Cauliflower', 100, 25, 0, 1, 4, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Celery', 100, 16, 0, 0, 2, 1, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Corn', 100, 86, 1, 3, 18, 2, 6
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Cucumber', 100, 15, 0, 0, 3, 0, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Eggplant', 100, 25, 0, 0, 5, 3, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Fennel', 100, 31, 0, 1, 7, 3, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Ginger Root', 100, 80, 0, 1, 17, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Kale', 100, 49, 0, 4, 8, 3, 2
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Leeks', 100, 61, 0, 1, 14, 1, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Iceberg Lettuce', 100, 14, 0, 0, 2, 1, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Green Leaf Lettuce', 100, 15, 0, 1, 2, 1, 0
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Red Leaf Lettuce', 100, 16, 0, 1, 2, 0 , 0
+);
+
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Romaine Lettuce', 100, 17, 0, 1, 3, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Portabella Mushrooms', 100, 22, 0, 2, 3, 1, 2
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Shiitake Mushrooms', 100, 34, 0, 2, 6, 2, 2
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'White Mushrooms', 100, 22, 0, 3, 3, 1, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Okra', 100, 33, 0, 1, 7, 3, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Onions', 100, 40, 0, 1, 9, 1, 4
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Green Peas', 100, 81, 0, 5, 14, 5, 5
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Red Peppers', 100, 31, 0, 0, 6, 2, 4
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Red Potato', 100, 70, 0, 1, 15, 1, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'White Potato', 100, 69, 0, 1, 15, 2, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Yellow Potato', 100, 79, 0, 2, 17, 2, 0
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Radishes', 100, 16, 0, 0, 3, 1, 1
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Spinach', 100, 23, 0, 2, 3, 2, 0
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Sweet Potato', 100, 86, 0, 1, 20, 3, 4
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Tomatillo', 100, 32, 1, 0, 5, 1, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Tomato', 100, 18, 0, 0, 3, 1, 2
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turnip', 100, 28, 0, 0, 6, 1, 3
+);
+
+INSERT INTO vegfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Zucchini', 100, 17, 0, 1, 3, 1, 2
+);
+
+SELECT * FROM vegfoodinfo;
