ea11b7dc10e39a7429f7a4d1a9a289f9858574b2
bigyihsuan <bigyihsuan@gmail.com>
2021-09-29 10:33:47 -0400

    message testing

---
+++ back/send.php
@@ -0,0 +1,24 @@
+<?php
+include("../servers.php");
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection($rabbit_server, 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$end = 5;
+for ($i = 0; $i < $end; $i++) {
+    $m = readline("Message $i: ");
+    $msg = new AMQPMessage("$i $m");
+    $channel->basic_publish($msg, '', 'hello');
+
+    echo " [$i/$end] Sent '$m'\n";
+}
+
+$channel->close();
+$connection->close();
