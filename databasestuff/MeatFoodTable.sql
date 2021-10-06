512a14754d0b2770ff4e2f3bf67f457add4d91d7
jr635 <70586888+jr635@users.noreply.github.com>
2021-10-06 11:08:23 -0400

    We Have The Meats

---
+++ databasestuff/MeatFoodTable.sql
@@ -0,0 +1,325 @@
+/* CREATE TABLE */
+CREATE TABLE meatfoodinfo (
+Food VARCHAR(100),
+Serving_Size INTEGER,
+Calories INTEGER,
+Fat INTEGER,
+Sugar INTEGER,
+Carbs INTEGER,
+Protein INTEGER,
+Fiber INTEGER
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Breast', 100, 172, 9, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Thigh', 100, 221, 16, 16, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Leg', 214, 15, 16, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Wing', 191, 12, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Gizzard', 100, 94, 2, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Liver', 119, 4, 16, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Heart', 100, 153, 9, 15, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Chicken Giblets', 100, 124, 4, 17, 1, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Ribeye Steak', 100, 180, 10, 19, 1, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Strip Steak', 100, 117, 2, 23, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Tenderloin Steak', 100, 139, 5, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Porterhouse Steak', 100, 247, 16, 24, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Skirt Steak', 100, 206, 14, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Flat Iron Steak', 100, 137, 6, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Flank Steak', 100, 141, 5, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Sirloin Steak', 100, 131, 4, 22, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Filet Mignon', 100, 267, 17, 26, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Italian Sausage', 100, 346, 31, 14, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Pork Sausage', 100, 231, 18, 14, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey Sausage', 100, 155, 8, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Polish Sausage', 100, 326, 28, 14, 1, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Vienna Sausage', 100, 230, 19, 10, 2, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Beef Sausage', 100, 332, 27, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Pork Loin', 100, 198, 12, 19, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(,
+'Pork Shoulder', 100, 236, 17, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Boston Butt', 100, 186, 12, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Pork Belly', 100, 518, 53, 9, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Leg of Ham', 100, 245, 18, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Pork Blade', 100, 194, 12, 19, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Pork Spareribs', 100, 277, 23, 15, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Beef Shortribs', 100, 388, 36, 14, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Ground Pork', 100, 263, 21, 16, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Beef Brisket', 100, 155, 7, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'70% Ground Beef', 100, 332, 30, 14, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'75% Ground Beef', 100, 293, 25, 15, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'80% Ground Beef', 100, 254, 20, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'85% Ground Beef', 100, 215, 15, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'90% Ground Beef', 100, 176, 10, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'95% Ground Beef', 100, 137, 5, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'97% Ground Beef', 100, 121, 3, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Goat', 100, 109, 2, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey', 100, 143, 5, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey Giblets', 100, 124, 5, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey Gizzard', 100, 111, 3, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey Heart', 100, 140, 7, 16, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Turkey Liver', 100, 128, 5, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Duck Breast', 100, 123, 4, 19, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Duck', 100, 211, 15, 17, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Veal Loin', 100, 177, 10, 20, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Veal Leg', 100, 107, 1, 21, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Veal Rib', 100, 162, 9, 18, 0, 0, 0
+);
+
+INSERT INTO meatfoodinfo(Food, Serving_Size, Calories, Fat, Protein, Carbs, Fiber, Sugar)
+VALUES
+(
+'Venison', 100, 116, 2, 21, 0, 0, 0
+);
+
+SELECT * FROM meatfoodinfo;
