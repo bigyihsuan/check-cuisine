02f1551290ac0064836630ad804fb0358eb52140
bigyihsuan <bigyihsuan@gmail.com>
2021-10-13 11:13:58 -0400

    sshpass

--- startup/master_startup.sh
+++ startup/master_startup.sh
@@ -11,7 +11,7 @@ if [ $? -eq 0 ]; then
     echo "${rabbit_server[1]} is running."
 else
     echo "${rabbit_server[1]} is not running."
-    ssh -l ${start_services_user[0]} ${rabbit_server[0]} 'sudo hamachi login;
+    sshpass -p ${start_services_user[1]} ssh -l ${start_services_user[0]} ${rabbit_server[0]} 'sudo hamachi login;
     sudo hamachi join it490-005-4 123;
     sudo service rabbitmq-server start && echo "[start-rabbit] RabbitMQ started" || echo "[start-rabbit] RabbitMQ startup failed";'
 
