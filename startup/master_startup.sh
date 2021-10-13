f58a43a1432c655a07153de790f7b84ff4378e9e
bigyihsuan <bigyihsuan@gmail.com>
2021-10-13 11:08:28 -0400

    test with rabbit

--- startup/master_startup.sh
+++ startup/master_startup.sh
@@ -4,8 +4,9 @@ front_server=("25.52.112.72" "apache2")           # yasu
 rabbit_server=("25.52.117.120" "rabbitmq-server") # jeff
 back_server=("25.52.190.242" "apache2")           # yi-hsuan
 database_server=("25.52.70.25" "mysql-server")    # jakub
+start_services_user=("start-services-user" "start-services")
 
-ssh "${rabbit_server[0]}" "ps cax | grep ${rabbit_server[1]} > /dev/null"
+sshpass -p ${start_services_user[1]} ssh -l ${start_services_user[0]} ${rabbit_server[0]} "ps cax | grep ${rabbit_server[1]} > /dev/null"
 if [ $? -eq 0 ]; then
     echo "${rabbit_server[1]} is running."
 else
