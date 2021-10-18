11362eb49fb7d07c43d713fd24a4dfc74076a313
bigyihsuan <bigyihsuan@gmail.com>
2021-10-17 21:26:17 -0400

    master startup

--- startup/master_startup.sh
+++ startup/master_startup.sh
@@ -4,12 +4,56 @@ front_server=("25.52.112.72" "apache2")           # yasu
 rabbit_server=("25.52.117.120" "rabbitmq-server") # jeff
 back_server=("25.52.190.242" "apache2")           # yi-hsuan
 database_server=("25.52.70.25" "mysql-server")    # jakub
-start_services_user=("start-services-user" "start-services")
+start_services_user="start-services-user"
 
-sshpass -p ${start_services_user[1]} ssh -l ${start_services_user[0]} ${rabbit_server[0]} "ps cax | grep ${rabbit_server[1]} > /dev/null"
+ssh -l ${start_services_user[0]} ${front_server[0]} "ps cax | grep ${front_server[1]} > /dev/null"
+if [ $? -eq 0 ]; then
+    echo "${database_server[1]} is running."
+else
+    echo "${front_server[1]} is not running."
+    ssh -l ${start_services_user[0]} ${front_server[0]} "sudo hamachi login;
+    sudo hamachi join it490-005-4 123;
+    sudo service ${front_server[1]} start
+    && echo '[start-rabbit] ${front_server[1]} started'
+    || echo '[start-rabbit] ${front_server[1]} startup failed';
+    exit"
+fi
+
+ssh -l ${start_services_user[0]} ${databrabbit_serverase_server[0]} "ps cax | grep ${database_server[1]} > /dev/null"
 if [ $? -eq 0 ]; then
     echo "${rabbit_server[1]} is running."
 else
     echo "${rabbit_server[1]} is not running."
-    sshpass -p ${start_services_user[1]} ssh -l ${start_services_user[0]} ${rabbit_server[0]} 'sudo hamachi login; sudo hamachi join it490-005-4 123; sudo service rabbitmq-server start && echo "[start-rabbit] RabbitMQ started" || echo "[start-rabbit] RabbitMQ startup failed"; exit'
+    ssh -l ${start_services_user[0]} ${rabbit_server[0]} "sudo hamachi login;
+    sudo hamachi join it490-005-4 123;
+    sudo service ${rabbit_server[1]} start
+    && echo '[start-rabbit] ${rabbit_server[1]} started'
+    || echo '[start-rabbit] ${rabbit_server[1]} startup failed';
+    exit"
+fi
+
+ssh -l ${start_services_user[0]} ${back_server[0]} "ps cax | grep ${back_server[1]} > /dev/null"
+if [ $? -eq 0 ]; then
+    echo "${back_server[1]} is running."
+else
+    echo "${database_server[1]} is not running."
+    ssh -l ${start_services_user[0]} ${back_server[0]} "sudo hamachi login;
+    sudo hamachi join it490-005-4 123;
+    sudo service ${back_server[1]} start
+    && echo '[start-rabbit] ${back_server[1]} started'
+    || echo '[start-rabbit] ${back_server[1]} startup failed';
+    exit"
+fi
+
+ssh -l ${start_services_user[0]} ${database_server[0]} "ps cax | grep ${database_server[1]} > /dev/null"
+if [ $? -eq 0 ]; then
+    echo "${database_server[1]} is running."
+else
+    echo "${database_server[1]} is not running."
+    ssh -l ${start_services_user[0]} ${database_server[0]} "sudo hamachi login;
+    sudo hamachi join it490-005-4 123;
+    sudo service ${database_server[1]} start
+    && echo '[start-rabbit] ${database_server[1]} started'
+    || echo '[start-rabbit] ${database_server[1]} startup failed';
+    exit"
 fi
