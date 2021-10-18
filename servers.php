1e23c56a0b7558c88289735578f57e735ca4296c
bigyihsuan <bigyihsuan@gmail.com>
2021-10-18 11:09:11 -0400

    update ips

--- servers.php
+++ servers.php
@@ -1,9 +1,9 @@
 <?php
 
-const front_server = "25.52.112.72"; // yasu
+const front_server = "25.53.112.72"; // yasu
 const rabbit_server = "25.77.32.88"; // jeff
-const back_server = "25.52.190.242"; // yi-hsuan
-const database_server = "25.52.70.25"; // jakub
+const back_server = "25.51.190.242"; // yi-hsuan
+const database_server = "25.8.159.27"; // jakub
 
 const back_server_creds = ["backend", "back"];
 const front_server_creds = ["frontend", "front"];
