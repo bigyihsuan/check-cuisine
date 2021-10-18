f6a2cf4df6725947f5241da0a706f9a124f108e7
jr635 <70586888+jr635@users.noreply.github.com>
2021-10-18 10:04:43 -0400

    Redo User Table code
    hopefully this should suffice for the time being

--- database/UserTable.sql
+++ database/UserTable.sql
@@ -1,7 +1,8 @@
 /* CREATE TABLE */
-CREATE TABLE User (
-    user_id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL,
-    pass VARCHAR(255) NOT NULL,
-    PRIMARY KEY (user_id)
-    );
+CREATE TABLE IF NOT EXISTS `Users` (
+	`id` INT NOT NULL AUTO_INCREMENT
+	,`email` VARCHAR(100) NOT NULL
+	,`password` VARCHAR(60) NOT NULL
+	,PRIMARY KEY (`id`)
+	,UNIQUE (`email`)
+	)
