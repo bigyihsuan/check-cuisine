14bc271b47244dbae86d22bfe29d4cc5d53a7bbc
bigyihsuan <bigyihsuan@gmail.com>
2021-10-18 10:20:15 -0400

    dirs

--- startup/mysql.sh
+++ startup/mysql.sh
@@ -4,4 +4,4 @@ sudo hamachi login
 sudo hamachi join it490-005-4 123
 sudo service mysql start && echo "[start-mysql] MySql started" || echo "[start-mysql] MySql startup failed"
 # sudo service apache2 start && echo "[start-mysql] apache2 started" || echo "[start-mysql] apache2 startup failed"
-php "../database/database.php"
+php "database/database.php"
