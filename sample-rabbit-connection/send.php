1ecae0abba11d891bdfcd0282228e5dc970385a1
bigyihsuan <bigyihsuan@gmail.com>
2021-10-20 10:06:41 -0400

    sample rabbiy

--- sample-rabbit-connection/send.php
+++ sample-rabbit-connection/send.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection($rabbit_server, 5672, $back_server_creds[0], $back_server_creds[1]);
+$connection = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
 $channel = $connection->channel();
 
 // queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
