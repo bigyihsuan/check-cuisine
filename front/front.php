288e6f9642f17def0b3e902136e6cce479a2814b
jy374 <90633673+jy374@users.noreply.github.com>
2021-10-30 14:28:50 -0400

    Update front.php

--- front/front.php
+++ front/front.php
@@ -55,8 +55,8 @@ $handle_back_to_front = function (AMQPMessage $message) {
     print("[FRONT] message = \"$body\"\n");
     print("[FRONT] finished\n");
 };
-boolean autoAck = true;
-$consume_channel->basic_consume(FRONT_BACK, "", autoAck, $handle_back_to_front);
+
+$consume_channel->basic_consume(FRONT_BACK, "", auto_ack = true, $handle_back_to_front);
 
 while ($consume_channel->is_open()) {
     $consume_channel->wait();
