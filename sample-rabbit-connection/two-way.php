efd5727c0dc9e5f5d3a95f3c7ed687eef536d4e8
bigyihsuan <bigyihsuan@gmail.com>
2021-10-29 21:35:40 -0400

    2 connections for 2 channels

--- sample-rabbit-connection/two-way.php
+++ sample-rabbit-connection/two-way.php
@@ -6,30 +6,34 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
-$channel = $connection->channel();
+$publish = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$consume = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$publish_channel = $publish->channel();
+$consume_channel = $consume->channel();
 
 // queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
-$channel->queue_declare('hello', false, true, false, false);
+$publish_channel->queue_declare('hello', false, true, false, false);
 
 if (isset($argv[1])) {
     $msg = new AMQPMessage($argv[1]);
-    $channel->basic_publish($msg, '', 'hello');
+    $publish_channel->basic_publish($msg, '', 'hello');
     echo "Sent '{$msg->getBody()}'\n";
 }
 
 echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function (AMQPMessage $msg) {
+    global $consume_channel;
+
     echo ' [x] Received ', $msg->body, "\n";
     $m = readline("Message: ");
     $msg = new AMQPMessage($m);
-    $msg->getChannel()->basic_publish($msg, '', 'hello');
+    $consume_channel->basic_publish($msg, '', 'hello');
     echo "Sent '$m'\n";
 };
 
 // basic_consume(queue name, consumer tag, no local?, no ack?, exclusive?, no wait?, callback)
-$channel->basic_consume('hello', '', false, true, false, false, $callback);
+$consume_channel->basic_consume('hello', '', false, true, false, false, $callback);
 
 while ($channel->is_open()) {
     $channel->wait();
