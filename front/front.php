e78f1f41614b6e23292e3970e5ab35083267d97b
jy374 <90633673+jy374@users.noreply.github.com>
2021-10-30 15:08:00 -0400

    Update front.php

--- front/front.php
+++ front/front.php
@@ -39,7 +39,7 @@ function run_query($prefix)
 
 // $backend_client = new Client($connection, FRONT_BACK);
 // $body = $backend_client->send_query($body, "");
-/*
+
 $body = readline("Enter message content: ");
 print("[FRONT] sending message to BACK...\n");
 print("[FRONT] message = \"$body\"\n");
@@ -55,8 +55,8 @@ $handle_back_to_front = function (AMQPMessage $message) {
     print("[FRONT] message = \"$body\"\n");
     print("[FRONT] finished\n");
 };
-*/
 
+/*
 $handle_back_to_front = function (AMQPMessage $message) {
     global $publish_channel;
     
@@ -64,13 +64,13 @@ $handle_back_to_front = function (AMQPMessage $message) {
     $body = readline("Enter message content: ");
     $message = new AMQPMessage($body);
     $publish_channel->basic_publish($message, "", FRONT_BACK);
-    /*
+    
     print("[FRONT] appending FRONT to message and printing...\n");
     $body .= "\nFRONT receieved";
-    */
+    
     print("[FRONT] message = \"$body\"\n");
     print("[FRONT] finished\n");    
-
+*/
 $consume_channel->basic_consume(FRONT_BACK, "", $handle_back_to_front);
 
 while ($consume_channel->is_open()) {
