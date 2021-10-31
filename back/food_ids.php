7bd7e2d2a9df2b4ff3ec2310c04843043ee5006f
bigyihsuan <bigyihsuan@gmail.com>
2021-10-31 18:30:06 -0400

    able get multiple foods

---
+++ back/food_ids.php
@@ -0,0 +1,23 @@
+<?php
+
+$food_ids = [
+    170327 => 'Burger King French Fries',
+    170328 => 'Burger King Hamburger',
+    170329 => 'Burger King Cheeseburger',
+    170727 => 'Burger King Whopper',
+    170728 => 'Burger King Whopper with Cheese',
+    170729 => 'Burger King Double Whopper',
+    170730 => 'Burger King Double Whopper with Cheese',
+    170731 => 'Burger King Chicken Strips',
+    170330 => 'Burger King Premium Fish Sandwich',
+    170700 => 'Burger King Vanilla Shake',
+];
+
+$nutrient_ids = [
+    1008 => "Energy",
+    1004 => "Total lipid (fat)",
+    1003 => "Protein",
+    1005 => "Carbohydrate, by difference",
+    1079 => "Fiber, total dietary",
+    2000 => "Sugars, total including NLEA",
+];
--- back/get_data.php
+++ back/get_data.php
@@ -3,55 +3,74 @@
 // https://fdc.nal.usda.gov/api-guide.html
 
 include_once __DIR__ . "/../servers.php";
+include_once __DIR__ . "/food_ids.php";
 
 $food_get = "https://api.nal.usda.gov/fdc/v1/food/";
+$foods_get = "https://api.nal.usda.gov/fdc/v1/foods/";
 $apple_no = "171515";
 $api_query = "?api_key=" . API_KEY_FDC;
 
-$query = $food_get . $apple_no . $api_query /*. "&format=abridged"*/;
+// $query = $food_get . $apple_no . $api_query /*. "&format=abridged"*/;
+
+
+$ids = "";
+foreach ($food_ids as $id => $_) {
+    $ids .= "&fdcIds=$id";
+}
+// $ids = substr($ids, 0, strlen($ids) - 1);
+// $nut_ids = "";
+// foreach ($nutrient_ids as $nut_id => $_) {
+//     $nut_ids .= "&nutrients=$nut_id";
+// }
+
+$query = $foods_get . $api_query . $ids  /* . $nut_ids */;
 
 echo $query . "\n";
 
 echo "[get_data] getting data...\n";
 $data = file_get_contents($query);
 echo "[get_data] received data\n";
+
+// file_put_contents("data.json", json_encode(json_decode($data, true), JSON_PRETTY_PRINT));
+
 $data = json_decode($data, true);
 
-$calories_name = "Energy";
-$fat_name = "Total lipid (fat)";
-$protein_name = "Protein";
-$carbs_name = "Carbohydrate, by difference";
-$fiber_name = "Fiber, total dietary";
-$sugar_name = "Sugars, total including NLEA";
+// $calories_name = "Energy";
+// $fat_name = "Total lipid (fat)";
+// $protein_name = "Protein";
+// $carbs_name = "Carbohydrate, by difference";
+// $fiber_name = "Fiber, total dietary";
+// $sugar_name = "Sugars, total including NLEA";
 
-$food_nutrients = $data['foodNutrients'];
-
-$nuts = array();
-
-foreach ($food_nutrients as $nutrient) {
-    switch ($nutrient['nutrient']['name']) {
-        case $calories_name:
-        case $fat_name:
-        case $protein_name:
-        case $carbs_name:
-        case $fiber_name:
-        case $sugar_name: {
-                $nut = new Nutrient(
-                    $nutrient['nutrient']['name'],
-                    $nutrient['amount'],
-                    $nutrient['nutrient']['unitName'],
-                );
-                $nuts[] = $nut;
-                break;
-            }
-        default: {
-                break;
-            }
+$foods = [];
+foreach ($data as $food) {
+    $food_nutrients = $food['foodNutrients'];
+    // echo "{$food["fdcId"]} {$food_ids[$food["fdcId"]]}\n";
+    $nuts = array();
+    foreach ($food_nutrients as $nutrient) {
+        if (array_key_exists($nutrient['nutrient']['id'], $nutrient_ids)) {
+            $nut = new Nutrient(
+                $nutrient['nutrient']['name'],
+                $nutrient['amount'],
+                $nutrient['nutrient']['unitName'],
+            );
+            // echo "{$nutrient['nutrient']['id']}\n";
+            $nuts[] = $nut;
+        }
     }
+
+    // foreach ($nuts as $nutrient) {
+    //     echo "{$nutrient->name} {$nutrient->amount} {$nutrient->unit}\n";
+    // }
+
+    $foods[] = new Food($food_ids[$food["fdcId"]], $nuts);
 }
 
-foreach ($nuts as $nutrient) {
-    echo "{$nutrient->name} {$nutrient->amount} {$nutrient->unit}\n";
+foreach ($foods as $food) {
+    echo "\n{$food->name}\n";
+    foreach ($nuts as $nutrient) {
+        echo "{$nutrient->name} {$nutrient->amount} {$nutrient->unit}\n";
+    }
 }
 
 // json structure:
@@ -87,5 +106,17 @@ class Nutrient
         $this->name = $name;
         $this->amount = $amount;
         $this->unit = $unit;
+    }
+}
+
+class Food
+{
+    public string $name;
+    public array $nutrients;
+
+    public function __construct(string $name, array $nutrients)
+    {
+        $this->name = $name;
+        $this->nutrients = $nutrients;
     }
 }
