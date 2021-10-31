808d5a37a085c9fa3ac37f19dae3ec300e4b79d9
jy374 <90633673+jy374@users.noreply.github.com>
2021-10-30 22:55:21 -0400

    Update frontsend.php

--- front/frontsend.php
+++ front/frontsend.php
@@ -12,23 +12,7 @@ $channel = $connection->channel();
 // queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
 $channel->queue_declare('FRONT_BACK', false, false, false, false);
 
-
-body = readline("Enter message content: ");
-print("[FRONT] sending message to BACK...\n");
-print("[FRONT] message = \"$body\"\n");
-$message = new AMQPMessage($body);
-$publish_channel->basic_publish($message, "", FRONT_BACK);
-
-$handle_back_to_front = function (AMQPMessage $message) {
-    print("[FRONT] received message from BACK!\n");
-    $body = $message->getBody();
-    print("[FRONT] appending FRONT to message and printing...\n");
-    $body .= "\nFRONT receieved";
-
-    print("[FRONT] message = \"$body\"\n");
-    print("[FRONT] finished\n");
-};
-/*
+$end = 1;
 for ($i = 0; $i < $end; $i++) {
     $m = readline("Message $i: ");
     $msg = new AMQPMessage("$i $m");
@@ -36,6 +20,6 @@ for ($i = 0; $i < $end; $i++) {
 
     echo " [$i/$end] Sent '$m'\n";
 }
-*/
+
 $channel->close();
 $connection->close();
