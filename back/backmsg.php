c16e59ae811ccab50874631717fdff0f352d3298
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-01 17:23:38 -0400

    Update backmsg.php

--- back/backmsg.php
+++ back/backmsg.php
@@ -40,11 +40,12 @@ $callback = function (AMQPMessage $msg) {
     //echo "Sent '$m'\n";
     
     $consume_channel->basic_publish($msg, '', 'back-data');
+    $consume_channel->basic_publish($msg2, '', 'back-data');
     
-    $m2 = readline("Message: ");
-    $msg2 = new AMQPMessage($m2);
-    $consumeData_channel->basic_publish($msg2, '', 'data-back');
-    echo "Sent '$m2'\n";
+    $m3 = readline("Message: ");
+    $msg3 = new AMQPMessage($m3);
+    $consumeData_channel->basic_publish($msg3, '', 'data-back');
+    echo "Sent '$m3'\n";
 
     //echo "Sent '$msg'\n";
 
