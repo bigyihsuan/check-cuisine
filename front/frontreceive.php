b4ff29214a179fd5b1a91a943eb4d96a03ad17d5
jy374 <90633673+jy374@users.noreply.github.com>
2021-10-31 21:36:14 -0400

    Create frontreceive.php

---
+++ front/frontreceive.php
@@ -0,0 +1,28 @@
+<?php
+include("servers.php");
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+$connection = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$channel = $connection->channel();
+
+$channel->queue_declare('front-back', false, false, false, false);
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function ($msg) {
+    echo ' [x] Received ', $msg->body, "\n";
+   
+};
+
+// basic_consume(queue name, consumer tag, no local?, no ack?, exclusive?, no wait?, callback)
+$channel->basic_consume('front-back', '', false, true, false, false, $callback);
+
+while ($channel->is_open()) {
+    $channel->wait();
+}
+
+$channel->close();
+$connection->close();
