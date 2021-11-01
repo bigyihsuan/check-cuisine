8783bc24c799c54fd2142182664ecacaf0743982
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-01 16:59:04 -0400

    Update datamsg.php

--- database/datamsg.php
+++ database/datamsg.php
@@ -10,13 +10,18 @@ $publish = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], b
 $consume = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
 $publish_channel = $publish->channel();
 $consume_channel = $consume->channel();
+$consumeData_channel = $consume->channel();
 
 // queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
-$publish_channel->queue_declare(FRONT_BACK, false, true, false, false);
+$publish_channel->queue_declare('front-send', false, true, false, false);
+$consume_channel->queue_declare('back-data', false, true, false, false);
+$consumeData_channel->queue_declare('data-back', false, true, false, false);
+
 
 if (isset($argv[1])) {
     $msg = new AMQPMessage($argv[1]);
-    $publish_channel->basic_publish($msg, '', BACK_DATA);
+    //$publish_channel->basic_publish($msg, '', FRONT_BACK);
+    $consume_channel->basic_publish($msg, '', BACK_DATA);
     echo "Sent '{$msg->getBody()}'\n";
 }
 
@@ -24,16 +29,29 @@ echo " [*] Waiting for messages. To exit press CTRL+C\n";
 
 $callback = function (AMQPMessage $msg) {
     global $publish_channel;
-
+    global $consume_channel;
+    global $consumeData_channel;
+    
     echo ' [x] Received ', $msg->body, "\n";
-    $m = readline("Message: ");
-    $msg = new AMQPMessage($m);
-    $publish_channel->basic_publish($msg, '', FRONT_BACK);
-    echo "Sent '$m'\n";
+    //$m = readline("Message: ");
+    //$msg = new AMQPMessage($m);
+    //$publish_channel->basic_publish($msg, '', BACK_DATA);
+    //$consume_channel->basic_publish($msg, '', BACK_DATA);
+    //echo "Sent '$m'\n";
+    
+    $consume_channel->basic_publish($msg, '', 'back-data');
+    
+    $m2 = readline("Message: ");
+    $msg2 = new AMQPMessage($m2);
+    $consumeData_channel->basic_publish($msg2, '', 'data-back');
+    echo "Sent '$m2'\n";
+
+    //echo "Sent '$msg'\n";
+
 };
 
 // basic_consume(queue name, consumer tag, no local?, no ack?, exclusive?, no wait?, callback)
-$consume_channel->basic_consume(FRONT_BACK, '', false, true, false, false, $callback);
+$consume_channel->basic_consume('front-send', '', false, true, false, false, $callback);
 
 while ($consume_channel->is_open()) {
     $consume_channel->wait();
