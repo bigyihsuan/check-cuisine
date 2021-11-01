3c4edf116670b68d910953fa605b5c9e20d919d1
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-01 11:11:01 -0400

    Update backmsg.php

--- back/backmsg.php
+++ back/backmsg.php
@@ -13,6 +13,7 @@ $consume_channel = $consume->channel();
 
 // queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
 $publish_channel->queue_declare(FRONT_BACK, false, true, false, false);
+$consume_channel->queue_declare(BACK_DATA, false, true, false, false);
 
 if (isset($argv[1])) {
     $msg = new AMQPMessage($argv[1]);
