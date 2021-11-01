9c31f7c0a816df6e00e57992a1fcfedb1f496ba6
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-01 12:02:03 -0400

    Create datamsg.php

---
+++ database/datamsg.php
@@ -0,0 +1,45 @@
+<?php
+include("servers.php");
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$publish = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$consume = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$publish_channel = $publish->channel();
+$consume_channel = $consume->channel();
+
+// queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
+$publish_channel->queue_declare(FRONT_BACK, false, true, false, false);
+
+if (isset($argv[1])) {
+    $msg = new AMQPMessage($argv[1]);
+    $publish_channel->basic_publish($msg, '', BACK_DATA);
+    echo "Sent '{$msg->getBody()}'\n";
+}
+
+echo " [*] Waiting for messages. To exit press CTRL+C\n";
+
+$callback = function (AMQPMessage $msg) {
+    global $publish_channel;
+
+    echo ' [x] Received ', $msg->body, "\n";
+    $m = readline("Message: ");
+    $msg = new AMQPMessage($m);
+    $publish_channel->basic_publish($msg, '', FRONT_BACK);
+    echo "Sent '$m'\n";
+};
+
+// basic_consume(queue name, consumer tag, no local?, no ack?, exclusive?, no wait?, callback)
+$consume_channel->basic_consume(FRONT_BACK, '', false, true, false, false, $callback);
+
+while ($consume_channel->is_open()) {
+    $consume_channel->wait();
+}
+
+$publish->close();
+$consume->close();
+$publish_channel->close();
+$consume_channel->close();
