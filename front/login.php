c13bac4dad25da2565c8b5e6a067e1b7981c1101
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-07 21:59:36 -0500

    Update login.php

--- front/login.php
+++ front/login.php
@@ -7,6 +7,59 @@ include_once "frontsend.php";
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+//////////////////////////////////////////////////
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection(rabbit_server, 5672, back_server_creds[0], back_server_creds[1]);
+$channel = $connection->channel();
+
+// queue_declare(name, passive?, durable?, exclusive?, auto_delete?, nowait?)
+$channel->queue_declare('front-send', false, true, false, false);
+
+$end = 2;
+for ($i = 0; $i < $end; $i++) {
+
+    if ($i = 1) {
+        $username = readline("Username: ");
+        $password = readline("Password: ");
+
+        $userpass = array($username => $password);
+        $userpass_json = json_encode($userpass);
+
+        $msg1 = new AMQPMessage($userpass_json);
+        $channel->basic_publish($msg1, '', 'front-send');
+
+        // $msg2 = new AMQPMessage($m);
+        // $channel->basic_publish($msg2, '', 'front-send');
+
+        $channel->queue_declare('front-receive', false, true, false, false);
+
+        echo "Sent login info to backend \n\n";
+
+        echo " [*] Receiving data. To exit press CTRL+C\n";
+
+        $callback = function ($msg) {
+            echo ' [x] Received ', $msg->body, "\n";
+        };
+
+        // basic_consume(queue name, consumer tag, no local?, no ack?, exclusive?, no wait?, callback)
+        $channel->basic_consume('front-receive', '', false, true, false, false, $callback);
+
+        while ($channel->is_open()) {
+            $channel->wait();
+        }
+    }
+
+    //echo "Sent login info to backend \n";
+}
+
+$channel->close();
+$connection->close();
+//////////////////////////////////////////////////
+
 // login as given user
 $result = run_query(Prefix::LOGIN);
 list(, $is_success) = explode(" ", $result, 2);
