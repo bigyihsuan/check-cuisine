602bd1bca059130d0956cd38154a30ae85edee03
bigyihsuan <bigyihsuan@gmail.com>
2021-11-17 11:39:16 -0500

    revert

--- front/frontsend.php
+++ front/frontsend.php
@@ -88,7 +88,7 @@ for ($i = 0; $i < $end; $i++) {
 
         while ($channel->is_open()) {
             error_log("waiting for message...");
-            // $channel->wait(null, true);
+            $channel->wait();
         }
 
         //echo "Sent login info to backend \n";
