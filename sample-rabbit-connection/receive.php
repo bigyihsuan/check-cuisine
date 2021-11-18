3c4bff17bab71f0102443662434c003c3bd11cec
jy374 <90633673+jy374@users.noreply.github.com>
2021-11-18 09:42:25 -0500

    Update receive.php

--- sample-rabbit-connection/receive.php
+++ sample-rabbit-connection/receive.php
@@ -22,7 +22,8 @@ $callback = function ($msg) {
 $channel->basic_consume('hello', '', false, true, false, false, $callback);
 
 while ($channel->is_open()) {
-    $channel->wait();
+    //$channel->wait();
+    $channel->close();
     
       if ($msgnum == 1) {
         $channel->close();
