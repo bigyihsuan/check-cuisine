c76ab1985fb01f8ada2d901a673144c36bd3f554
bigyihsuan <bigyihsuan@gmail.com>
2021-11-29 11:15:33 -0500

    move connectio out of thing

--- database/db.php
+++ database/db.php
@@ -4,12 +4,13 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 global $db;
-if (!isset($db)) {
-    try {
-        $db = pg_connect("host=node87495-checkcuisinedb.ny-2.paas.massivegrid.net port=5432 dbname=usersdb user=webadmin password=1pQCRimGg5");
-        // $db = new PDO($conn, $dbuser, $dbpass);
-    } catch (Exception $e) {
-        var_export($e);
-        $db = null;
-    }
-}
+// if (!isset($db)) {
+//     try {
+//         $db = pg_connect("host=node87495-checkcuisinedb.ny-2.paas.massivegrid.net port=5432 dbname=usersdb user=webadmin password=1pQCRimGg5");
+//         // $db = new PDO($conn, $dbuser, $dbpass);
+//     } catch (Exception $e) {
+//         var_export($e);
+//         $db = null;
+//     }
+// }
+$db = pg_connect("host=node87495-checkcuisinedb.ny-2.paas.massivegrid.net port=5432 dbname=usersdb user=webadmin password=1pQCRimGg5");
