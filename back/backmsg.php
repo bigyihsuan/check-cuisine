8f54af44875d92c8162ae83890a4cb6cf0dfbf00
jy374 <90633673+jy374@users.noreply.github.com>
2021-12-01 10:59:28 -0500

    Update backmsg.php

--- back/backmsg.php
+++ back/backmsg.php
@@ -99,7 +99,8 @@ $consume_channel->basic_consume('front-send', '', false, true, false, false, $ca
 
         while ($consumeReturn_channel->is_open()) {
             $consumeReturn_channel->wait();
-            $consumeReturn_channel->close();
+            $channel->close();
+            $connection->close();
         }
         ///-----------------------//////
 
