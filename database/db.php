7a9c4fdf0522925ec5f32cb07d01ef80c694433c
jr635 <70586888+jr635@users.noreply.github.com>
2021-12-15 19:46:29 -0500

    test test test

--- database/db.php
+++ database/db.php
@@ -3,16 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// if (!isset($db)) {
-//     try {
-//         $db = new mysqli('25.53.49.9', 'webadmin', '123', 'usersdb');
-//         // $db = new PDO($conn, $dbuser, $dbpass);
-//     } catch (Exception $e) {
-//         var_export($e);
-//         $db = null;
-//     }
-// }
-$db = new mysqli('localhost', 'webadmin', '123', 'usersdb');
+$db = mysqli_connect('localhost', 'webadmin', '123');
+mysqli_select_db($db, 'usersdb');
 if(mysqli_connect_errno()){
 exit();
 }
