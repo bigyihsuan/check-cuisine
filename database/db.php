d6e10548e5334cbc7b98bdf3a1be2685906bc7c9
jr635 <70586888+jr635@users.noreply.github.com>
2021-12-15 20:47:10 -0500

    moving code

--- database/db.php
+++ database/db.php
@@ -5,6 +5,20 @@ error_reporting(E_ALL);
 
 $db = mysqli_connect('localhost', 'webadmin', '123');
 mysqli_select_db($db, 'usersdb');
+
+$s = "SELECT * FROM users WHERE username = '$username'";
+    
+$result = mysqli_query($db, $s);
+$num = mysqli_num_rows($result);
+    
+if($num == 1){
+    echo "Username Already Taken";
+}else{
+    $reg = "INSERT INTO users(name, password) VALUES ('$username', '$password')";
+    mysqli_query($db, $reg);
+    echo "Registration Successful";
+ }
+
 if(mysqli_connect_errno()){
 exit();
 }
