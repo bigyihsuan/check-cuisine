623154dd7b810c39f1a8d4778ffacb614690a1b1
jr635 <70586888+jr635@users.noreply.github.com>
2021-12-15 20:54:52 -0500

    test

--- database/db.php
+++ database/db.php
@@ -4,17 +4,20 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $db = mysqli_connect('localhost', 'webadmin', '123');
-mysqli_select_db($db, 'usersdb');
+mysqli_select_db('$db', 'usersdb');
 
 $s = "SELECT * FROM users WHERE username = '$username'";
     
 $result = mysqli_query($db, $s);
 $num = mysqli_num_rows($result);
-    
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
 if($num == 1){
     echo "Username Already Taken";
 }else{
-    $reg = "INSERT INTO users(name, password) VALUES ('$username', '$password')";
+    $reg = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
     mysqli_query($db, $reg);
     echo "Registration Successful";
  }
