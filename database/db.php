b62010a58de6818d4aa9c43dcebed0d9ff989539
jr635 <70586888+jr635@users.noreply.github.com>
2021-12-15 22:08:13 -0500

    f
    f

--- database/db.php
+++ database/db.php
@@ -8,9 +8,6 @@ error_reporting(E_ALL);
 $db = mysqli_connect('localhost', 'webadmin', '123');
 mysqli_select_db($db, 'usersdb');
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
 $s = "SELECT * FROM users WHERE username = '$username'";
 
 $result = mysqli_query($db, $s);
