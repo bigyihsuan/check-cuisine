804c2a70c12d16fe78e3349265a7744c2c749b19
jr635 <70586888+jr635@users.noreply.github.com>
2021-12-19 19:33:06 -0500

    bing bong

--- database/db.php
+++ database/db.php
@@ -4,18 +4,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$db = mysqli_connect('localhost', 'webadmin', '123', 'usersdb');
+$host = "localhost";
+$user = "webadmin";
+$password = "123";
+$db = "usersdb";
+
+$con = mysqli_connect($host,$user,$password,$db);
 
 $s = "SELECT * FROM users WHERE username = '$username'";
 
-$result = mysqli_query($db, $s);
+$result = mysqli_query($con, $s);
 $num = mysqli_num_rows($result);
 
 if ($num == 1) {
     echo "Username Already Taken";
 } else {
     $reg = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
-    mysqli_query($db, $reg);
+    mysqli_query($con, $reg);
     echo "Registration Successful";
 }
 
