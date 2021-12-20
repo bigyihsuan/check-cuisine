ef694fca018edbe748c43af26f4cc36478d9c447
bigyihsuan <bigyihsuan@gmail.com>
2021-12-19 20:11:40 -0500

    match brackets

--- database/dbUses.php
+++ database/dbUses.php
@@ -1,14 +1,15 @@
 <?php
 //Require Once All Other Necessary Rabbit Files
-require_once ('db.php');
+require_once('db.php');
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function register($username, $password){
+function register($username, $password)
+{
    $con = dbCon();
-  
+
    $check = "SELECT * FROM users WHERE username = '$username'";
 
    $result = mysqli_query($con, $check);
@@ -16,21 +17,24 @@ function register($username, $password){
 
    if ($num == 0) {
       return true; //username available
-   }else{
+   } else {
       return false; //username not available
    }
-   
+
    $hashedpass = passHash($password);
    $reg = "INSERT INTO users(username, hashedpass) VALUES ('$username', '$hashedpass')";
    $query_status = mysqli_query($con, $reg);
    echo $query_status ? "Registration Successful" : "Registration failed";
+}
 
-function login($username, $password){
+function login($username, $password)
+{
    $con = dbCon();
 }
 
-function passHash($password){
-	$new = $password . 'abcdefghjiklmaopqrstuvwxyz1234567890';
-	$hash = hash('sha256',$new);
-	return $hash;
+function passHash($password)
+{
+   $new = $password . 'abcdefghjiklmaopqrstuvwxyz1234567890';
+   $hash = hash('sha256', $new);
+   return $hash;
 }
